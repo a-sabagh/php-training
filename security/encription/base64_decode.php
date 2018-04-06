c57ad3245d48eab0c01fe8d607d7484f82baa383
asabagh <a.sabagh72@gmail.com>
2018-04-06 16:06:59 +0430

    remove base64 function from string functions

--- security/encription/base64_decode.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string = "abolfazlsabagh";
-$base64 = base64_encode($string);
-$string = base64_decode($base64);
-echo "String(decode): " . $string ."<br>";
-echo "base64(encode): " . $base64 ."<br>";
-// string  -> encode -> base64 -> decode -> string
--- string/base64_decode.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="">
-	<textarea name="test"></textarea>
-	<input type="submit" name="submit" value="submit">
-</form>
-<?php
-if(isset($_POST['submit'])){
-	$data = $_POST['test'];
-	echo base64_decode($data);
-}
