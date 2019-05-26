fc51a1368c6380f7126fda88a26dffd60a143e69
Abolfazl Sabagh <a.sabagh72@gmail.com>
2019-05-26 12:46:11 +0430

    define constants

--- msqli/objective/mysqli_connect.php
+++ msqli/objective/mysqli_connect.php
@@ -1,15 +1,13 @@
 <?php
-define(HOST,'localhost');
-define(USER, 'root');
-define(PASSWORD, 'root');
-define(DATABASE, 'wpdb');
-define(PORT, 8888);
+define('HOST','localhost');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('DATABASE', 'wpdb');
+define('PORT', 8888);
 $connection = mysqli_connect(HOST , USER , PASSWORD , DATABASE , PORT);
 if($connection->connect_error){
     echo "error with connectiong mysql database: " . $connection->connect_error;
 }else{
-    echo '<pre>';
     var_dump($connection);
-    echo '<pre>';
 }
 
