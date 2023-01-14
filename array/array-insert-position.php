2b09003147f6dc96c905d64f607e14b06dfea2e5
Abolfazl Sabagh <a.sabagh72@gmail.com>
2023-01-14 23:25:07 +0330

    insert new item in array on any position

---
+++ array/array-insert-position.php
@@ -0,0 +1,14 @@
+<?php
+
+function array_insert(&$array, $position, $insert){
+    if (is_int($position)) {
+        array_splice($array, $position, 0, $insert);
+    } else {
+        $pos   = array_search($position, array_keys($array));
+        $array = array_merge(
+            array_slice($array, 0, $pos),
+            $insert,
+            array_slice($array, $pos)
+        );
+    }
+}
